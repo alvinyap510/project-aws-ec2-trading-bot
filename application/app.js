021e323c7ea9a4dc857311ae8ec9052bd71dc1d2
Alvin Yap <leaf.alvin@gmail.com>
2023-12-12 22:10:54 +0800

    code: add application

---
+++ application/app.js
@@ -0,0 +1,15 @@
+/*** Imports ***/
+require("dotenv").config();
+const axios = require("axios");
+const { RestClientV5 } = require("bybit-api");
+
+/*** ENV ***/
+const API_KEY = process.env.BYBIT_API_KEY;
+
+/*** Sub Functions ***/
+
+/*** Main ***/
+
+function main() {}
+
+main();
